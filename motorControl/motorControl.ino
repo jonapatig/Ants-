1c41a4c4a413a78a8278bfd69a241aabe2282c70
WillemPaternotte <willempaternotte@gmail.com>
2024-06-18 16:44:49 +0200

    MotorSpeedController + RPM class in MotorControl

--- motorControl/motorControl.ino
+++ motorControl/motorControl.ino
@@ -1,6 +1,25 @@
+#include "RPM/RPM.h"
 #define motorApwm 6
 #define motorAin1 5
 #define motorAin2 4
+#define RPMcounterInertupt 2
+
+RPMcounter RPM(RPMcounterInertupt);
+
+float setRPM = 0;
+
+//PID params
+float Kbase = 1;
+float Kp= 1;
+float Ki = 1;
+float Kd = 1;
+
+int motorSpeedController(float setRPM, float measuredRPM){
+  int controlValue = Kbase*setRPM
+  float error = setRPM- measuredRPM
+  controlValue += error*Kp
+  return  constrain(controlValue, 0, 255)
+}
 
 void setMotorDirection(int in1, int in2, bool direction=false){
   digitalWrite(in1, direction);
@@ -20,14 +39,17 @@ void setupMotor(int pwm, int in1,  int in2){
 void setup() {
   // put your setup code here, to run once:
   setupMotor(motorApwm, motorAin1, motorAin2);
+  RPMcounter.setup()
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 0; i < 255; i+=1){
-    analogWrite(motorApwm, i);
-    delay(100);
-    Serial.print(i);
-  }
+  RPMcounter.checkResetRPM();
+  if(Serial.available()){
+    setRPM = Serial.read();
+  }  
+  analogWrite(motorApwm, motorSpeedController(RPMcounter.RPM))
+  Serial.println(setRPM + ", " + RPMcounter.RPM )
+  delay(50);
 }
