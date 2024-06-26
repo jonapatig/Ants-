c36cc469d41718a81114462bf9ca9b5ff1490368
WillemPaternotte <willempaternotte@gmail.com>
2024-06-26 14:10:11 +0200

    test openCV

--- PI-test/test.py
+++ PI-test/test.py
@@ -4,7 +4,31 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 '''
 import serial
 import time
+import cv2
+# Load an image from file
+image_path = 'PI-test\Schermafbeelding 2023-11-08 222452.png'  # Replace with the actual path to your image file
+image = cv2.imread(image_path)
 
+if image is None:
+    print("Error: Could not load image.")
+else:
+    print("Image loaded successfully.")
+
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    print("Image converted to grayscale.")
+
+    # Display the original image
+    cv2.imshow('Original Image', image)
+    cv2.waitKey(0)
+
+    # Display the grayscale image
+    cv2.imshow('Grayscale Image', gray_image)
+    cv2.waitKey(0)
+
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
+    
 try:
     ser = serial.Serial('/dev/ttyACM0', 9600)  # Update with correct device name and baud rate
     time.sleep(2)  # Wait for the connection to establish
