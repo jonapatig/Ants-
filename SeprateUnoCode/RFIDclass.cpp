b0b8def3524b4f7aec77732f953a60d963de663e
WillemPaternotte <willempaternotte@gmail.com>
2024-06-27 12:01:04 +0200

    Nicer debug messaging

--- SeprateUnoCode/RFIDclass.cpp
+++ SeprateUnoCode/RFIDclass.cpp
@@ -18,8 +18,7 @@ void RFIDclass::setup() {
 void RFIDclass::main() {
     bool cardDetected = mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial();
 
-    Serial.print("Is ant Detected: ");
-    Serial.print(cardDetected);
+    
     if (cardDetected) {
         // If a card is detected, check how long it has been present
         lastDetectedTime = millis(); // Update the last detected time
@@ -28,8 +27,8 @@ void RFIDclass::main() {
             cardPresent = true; // Set the card present flag
 
             int cardNumber = getCardNumber();
-            Serial.print("The ant: ")
-            Serial.print(cardNumber);
+            Serial.print("The ant: ");
+            Serial.println(cardNumber);
             digitalWrite(card1Pin, cardNumber == 1 ? HIGH : LOW);
             digitalWrite(card2Pin, cardNumber == 2 ? HIGH : LOW);
             digitalWrite(card3Pin, cardNumber == 3 ? HIGH : LOW);
@@ -37,8 +36,10 @@ void RFIDclass::main() {
             
         }
     } else {
+        
         // No card detected
         if (cardPresent && millis() - lastDetectedTime > debounceDelay) {
+            Serial.println("No Ant");
             // Card was previously detected but is now absent for longer than debounceDelay
             cardPresent = false;
             digitalWrite(card1Pin, LOW);
@@ -46,7 +47,7 @@ void RFIDclass::main() {
             digitalWrite(card3Pin, LOW);
         }
     }
-    Serial.println();
+
 }
 
 int RFIDclass::getCardNumber() {
